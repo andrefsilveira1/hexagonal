d7a3921e6a26d01a7224eacaad5e177a28f385f4
André Freitas <freitasandre38@gmail.com>
2024-07-05 20:40:31 -0300

    initial steps

---
+++ application/product.go
@@ -0,0 +1,16 @@
+package application
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetId() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float32
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
