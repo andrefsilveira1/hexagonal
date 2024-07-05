4ba309f5131a73c975cfeeade97a817b40349f38
André Freitas <freitasandre38@gmail.com>
2024-07-05 20:43:48 -0300

    feat: implemets methods and interface from product

--- application/product.go
+++ application/product.go
@@ -14,3 +14,37 @@ const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
 )
+
+type Product struct {
+	Id     string
+	Name   string
+	Price  float32
+	Status string
+}
+
+func (p *Product) IsValid() (bool, error) {
+	return false, nil
+}
+
+func (p *Product) Enable() error {
+	return nil
+}
+
+func (p *Product) Disable() error {
+	return nil
+}
+
+func (p *Product) GetId() string {
+	return ""
+}
+
+func (p *Product) GetName() string {
+	return ""
+}
+
+func (p *Product) GetStatus() string {
+	return ""
+}
+func (p *Product) GetPrice() float32 {
+	return 0
+}
