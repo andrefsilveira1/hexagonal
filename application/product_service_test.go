939f4906b4f9258e2aa561738b72591de8faea1c
André Freitas <freitasandre38@gmail.com>
2024-07-06 06:49:22 -0300

    feat: create test using mock and gomock gen for product service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,27 @@
 package application_test
+
+import (
+	"testing"
+
+	"github.com/andrefsilveira1/hexagonal/application"
+	mock_application "github.com/andrefsilveira1/hexagonal/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Product_Service_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("aaa")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
